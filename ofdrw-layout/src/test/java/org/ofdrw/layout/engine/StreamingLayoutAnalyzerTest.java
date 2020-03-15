c6277c279999d00665fc4ab2b91887a23de89bab
quanguanyu@qq.com <005006007>
2020-03-15 14:29:13 +0800

    编写了部分测试用例

--- ofdrw-layout/src/test/java/org/ofdrw/layout/engine/StreamingLayoutAnalyzerTest.java
+++ ofdrw-layout/src/test/java/org/ofdrw/layout/engine/StreamingLayoutAnalyzerTest.java
@@ -1,7 +1,10 @@
 package org.ofdrw.layout.engine;
 
 import org.junit.jupiter.api.Test;
+import org.ofdrw.layout.PageLayout;
 
+import java.util.LinkedList;
+import java.util.List;
 
 
 class StreamingLayoutAnalyzerTest {
@@ -9,5 +12,11 @@ class StreamingLayoutAnalyzerTest {
     @Test
     public void analyze(){
         // TODO 分析
+        PageLayout layout = new PageLayout(210d, 297d);
+        StreamingLayoutAnalyzer analyzer = new StreamingLayoutAnalyzer(layout);
+        double width = layout.getWorkerArea().getWidth();
+        System.out.println(width);
+        List<Segment> seq = new LinkedList<>();
+
     }
 }
