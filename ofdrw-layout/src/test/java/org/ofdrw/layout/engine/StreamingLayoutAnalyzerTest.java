a0913e72d713a3ff6e8fed0c2adc0be06be36259
cliven <quanguanyu@qq.com>
2020-03-16 11:39:22 +0800

    增加了跨页的定位的测试

--- ofdrw-layout/src/test/java/org/ofdrw/layout/engine/StreamingLayoutAnalyzerTest.java
+++ ofdrw-layout/src/test/java/org/ofdrw/layout/engine/StreamingLayoutAnalyzerTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.ofdrw.layout.PageLayout;
 import org.ofdrw.layout.VirtualPage;
+import org.ofdrw.layout.element.Clear;
 import org.ofdrw.layout.element.Div;
+import org.ofdrw.layout.element.PageAreaFiller;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -17,8 +19,6 @@ class StreamingLayoutAnalyzerTest {
         // h: 225 w: 138
         PageLayout layout = new PageLayout(210d, 297d);
         StreamingLayoutAnalyzer analyzer = new StreamingLayoutAnalyzer(layout);
-        double width = layout.getWorkerArea().getWidth();
-        System.out.println(width);
         List<Segment> seq = new LinkedList<>();
 
         Segment sgm = new Segment(layout.contentWidth());
@@ -45,8 +45,14 @@ class StreamingLayoutAnalyzerTest {
         // h: 225 - 25 = 200
         seq.clear();
         sgm = new Segment(layout.contentWidth());
-        sgm.tryAdd(new Div(18d, 225d));
+        // 段内包含和分段和不可分段元素
+        sgm.tryAdd(new Div(50d, 50d).setClear(Clear.none));
+        sgm.tryAdd(new Div(50d, 225d).setClear(Clear.none).setIntegrity(true));
         seq.add(sgm);
         virtualPages = analyzer.analyze(seq);
+        Assertions.assertEquals(virtualPages.size(), 3);
+        Assertions.assertEquals(virtualPages.get(2).getContent().size(), 1);
+        div = virtualPages.get(2).getContent().get(0);
+        Assertions.assertEquals(div.getX(), 36 + 50);
     }
 }
