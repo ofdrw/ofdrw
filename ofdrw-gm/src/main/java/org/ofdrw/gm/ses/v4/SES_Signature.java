ef7cdbf03a667d582f6b3e1669b78b81f1aaf4a6
quanguanyu@qq.com <005006007>
2020-04-19 22:36:00 +0800

    完成V4版本的电子签章数据结构构造

---
+++ ofdrw-gm/src/main/java/org/ofdrw/gm/ses/v4/SES_Signature.java
@@ -0,0 +1,127 @@
+package org.ofdrw.gm.ses.v4;
+
+import org.bouncycastle.asn1.*;
+
+import java.util.Enumeration;
+
+/**
+ * 电子签章数据
+ *
+ * @author 权观宇
+ * @since 2020-04-19 22:04:04
+ */
+public class SES_Signature extends ASN1Object {
+
+    /**
+     * 签章信息
+     */
+    private TBS_Sign toSign;
+
+    /**
+     * 签章者证书
+     */
+    private ASN1OctetString cert;
+
+    /**
+     * 签名算法标识
+     */
+    private ASN1ObjectIdentifier signatureAlgID;
+
+    /**
+     * 签名值
+     */
+    private ASN1BitString signature;
+
+    /**
+     * 对签名值的时间戳【可选】
+     */
+    private ASN1BitString timeStamp;
+
+    public SES_Signature(TBS_Sign toSign,
+                         ASN1OctetString cert,
+                         ASN1ObjectIdentifier signatureAlgID,
+                         ASN1BitString signature,
+                         ASN1BitString timeStamp) {
+        this.toSign = toSign;
+        this.cert = cert;
+        this.signatureAlgID = signatureAlgID;
+        this.signature = signature;
+        this.timeStamp = timeStamp;
+    }
+
+    public SES_Signature(ASN1Sequence seq) {
+        Enumeration<?> e = seq.getObjects();
+        toSign = TBS_Sign.getInstance(e.nextElement());
+        cert = ASN1OctetString.getInstance(e.nextElement());
+        signatureAlgID = ASN1ObjectIdentifier.getInstance(e.nextElement());
+        signature = DERBitString.getInstance(e.nextElement());
+        if (e.hasMoreElements()) {
+            timeStamp = DERBitString.getInstance(e.nextElement());
+        }
+    }
+
+    public static SES_Signature getInstance(Object o) {
+        if (o instanceof SES_Signature) {
+            return (SES_Signature) o;
+        } else if (o != null) {
+            return new SES_Signature(ASN1Sequence.getInstance(o));
+        }
+        return null;
+    }
+
+    public TBS_Sign getToSign() {
+        return toSign;
+    }
+
+    public void setToSign(TBS_Sign toSign) {
+        this.toSign = toSign;
+    }
+
+    public ASN1OctetString getCert() {
+        return cert;
+    }
+
+    public void setCert(ASN1OctetString cert) {
+        this.cert = cert;
+    }
+
+    public ASN1ObjectIdentifier getSignatureAlgID() {
+        return signatureAlgID;
+    }
+
+    public void setSignatureAlgID(ASN1ObjectIdentifier signatureAlgID) {
+        this.signatureAlgID = signatureAlgID;
+    }
+
+    public ASN1BitString getSignature() {
+        return signature;
+    }
+
+    public void setSignature(ASN1BitString signature) {
+        this.signature = signature;
+    }
+    public void setSignature(byte[] signature) {
+        this.signature = new DERBitString(signature);
+    }
+
+    public ASN1BitString getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(ASN1BitString timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    @Override
+    public ASN1Primitive toASN1Primitive() {
+        ASN1EncodableVector v = new ASN1EncodableVector(5);
+        v.add(toSign);
+        v.add(cert);
+        v.add(signatureAlgID);
+        v.add(signature);
+        if (timeStamp != null) {
+            v.add(timeStamp);
+        }
+        return new BERSequence(v);
+    }
+}
---
+++ ofdrw-gm/src/main/java/org/ofdrw/gm/ses/v4/TBS_Sign.java
@@ -0,0 +1,153 @@
+package org.ofdrw.gm.ses.v4;
+
+import org.bouncycastle.asn1.*;
+import org.ofdrw.gm.ses.v1.ExtensionDatas;
+
+import java.util.Enumeration;
+
+/**
+ * 签章信息
+ *
+ * @author 权观宇
+ * @since 2020-04-19 21:29:45
+ */
+public class TBS_Sign extends ASN1Object {
+
+    /**
+     * 电子印章版本号，与电子印章版本号保持一致
+     */
+    private ASN1Integer version;
+
+    /**
+     * 电子印章
+     */
+    private SESeal eseal;
+
+    /**
+     * 签章时间
+     */
+    private ASN1GeneralizedTime timeInfo;
+
+    /**
+     * 原文杂凑值
+     */
+    private ASN1BitString dataHash;
+
+    /**
+     * 原文数据的属性
+     */
+    private DERIA5String propertyInfo;
+
+    /**
+     * 自定义数据 【可选】
+     */
+    private ExtensionDatas extDatas;
+
+    public TBS_Sign(ASN1Integer version,
+                    SESeal eseal,
+                    ASN1GeneralizedTime timeInfo,
+                    ASN1BitString dataHash,
+                    DERIA5String propertyInfo,
+                    ExtensionDatas extDatas) {
+        this.version = version;
+        this.eseal = eseal;
+        this.timeInfo = timeInfo;
+        this.dataHash = dataHash;
+        this.propertyInfo = propertyInfo;
+        this.extDatas = extDatas;
+    }
+
+    public TBS_Sign(ASN1Sequence seq) {
+        Enumeration<?> e = seq.getObjects();
+        version = ASN1Integer.getInstance(e.nextElement());
+        eseal = SESeal.getInstance(e.nextElement());
+        timeInfo = ASN1GeneralizedTime.getInstance(e.nextElement());
+        dataHash = DERBitString.getInstance(e.nextElement());
+        propertyInfo = DERIA5String.getInstance(e.nextElement());
+        if (e.hasMoreElements()) {
+            extDatas = ExtensionDatas.getInstance(e.nextElement());
+        }
+    }
+
+    public static TBS_Sign getInstance(Object o) {
+        if (o instanceof TBS_Sign) {
+            return (TBS_Sign) o;
+        } else if (o != null) {
+            return new TBS_Sign(ASN1Sequence.getInstance(o));
+        }
+        return null;
+    }
+
+    public ASN1Integer getVersion() {
+        return version;
+    }
+
+    public TBS_Sign setVersion(ASN1Integer version) {
+        this.version = version;
+        return this;
+    }
+
+    public SESeal getEseal() {
+        return eseal;
+    }
+
+    public TBS_Sign setEseal(SESeal eseal) {
+        this.eseal = eseal;
+        return this;
+    }
+
+    public ASN1GeneralizedTime getTimeInfo() {
+        return timeInfo;
+    }
+
+    public TBS_Sign setTimeInfo(ASN1GeneralizedTime timeInfo) {
+        this.timeInfo = timeInfo;
+        return this;
+    }
+
+    public ASN1BitString getDataHash() {
+        return dataHash;
+    }
+
+    public TBS_Sign setDataHash(ASN1BitString dataHash) {
+        this.dataHash = dataHash;
+        return this;
+    }
+
+    public TBS_Sign setDataHash(byte[] dataHash) {
+        this.dataHash = new DERBitString(dataHash);
+        return this;
+    }
+
+    public DERIA5String getPropertyInfo() {
+        return propertyInfo;
+    }
+
+    public TBS_Sign setPropertyInfo(DERIA5String propertyInfo) {
+        this.propertyInfo = propertyInfo;
+        return this;
+    }
+
+    public ExtensionDatas getExtDatas() {
+        return extDatas;
+    }
+
+    public TBS_Sign setExtDatas(ExtensionDatas extDatas) {
+        this.extDatas = extDatas;
+        return this;
+    }
+
+    @Override
+    public ASN1Primitive toASN1Primitive() {
+        ASN1EncodableVector v = new ASN1EncodableVector(6);
+        v.add(version);
+        v.add(eseal);
+        v.add(timeInfo);
+        v.add(dataHash);
+        v.add(propertyInfo);
+        if (extDatas != null) {
+            v.add(propertyInfo);
+        }
+        return new BERSequence(v);
+    }
+}
