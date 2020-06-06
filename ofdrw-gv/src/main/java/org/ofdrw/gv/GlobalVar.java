6497af7dc291d1516ae144762395c73b159421c8
quanguanyu@qq.com <005006007>
2020-06-06 11:08:29 +0800

    - CVE-2020-10683 dom4j库可能造成XXE 攻击，升级该库至2.1.3。 - 增加了替换附件的功能。

--- ofdrw-gv/src/main/java/org/ofdrw/gv/GlobalVar.java
+++ ofdrw-gv/src/main/java/org/ofdrw/gv/GlobalVar.java
@@ -11,5 +11,5 @@ final public class GlobalVar {
     /**
      * OFD Reader and Writer 版本号
      */
-    public static final String Version = "1.5.0";
+    public static final String Version = "1.5.1";
 }
