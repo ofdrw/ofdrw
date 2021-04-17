1ba9758db3a4e6dc9ff4f064f7b0c6f4194a23d0
cliven <quanguanyu@qq.com>
2021-04-17 23:43:45 +0800

    错误的包导入

--- ofdrw-converter/src/main/java/org/ofdrw/converter/PdfboxMaker.java
+++ ofdrw-converter/src/main/java/org/ofdrw/converter/PdfboxMaker.java
@@ -1,11 +1,6 @@
 package org.ofdrw.converter;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import org.apache.fontbox.ttf.TrueTypeFont;
-import org.apache.pdfbox.jbig2.JBIG2ImageReader;
-import org.apache.pdfbox.jbig2.JBIG2ImageReaderSpi;
-import org.apache.pdfbox.jbig2.io.DefaultInputStreamFactory;
-
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
