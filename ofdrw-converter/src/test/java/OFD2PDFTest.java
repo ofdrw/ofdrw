ab3261c2b1dc6fa3d9ebfc3fbe17af755583b30e
cliven <quanguanyu@qq.com>
2021-06-11 19:18:44 +0800

    修复 判断条件 导致的越界

--- ofdrw-converter/src/test/java/OFD2PDFTest.java
+++ ofdrw-converter/src/test/java/OFD2PDFTest.java
@@ -32,7 +32,7 @@ public class OFD2PDFTest {
 //            ConvertHelper.toPdf(src, dst);
 //            ConvertHelper.toPdf(Paths.get("src/test/resources/signout.ofd"), Paths.get("target/signout.pdf"));
 //            ConvertHelper.toPdf(Paths.get("src/test/resources/n.ofd"), Paths.get("target/n.pdf"));
-            ConvertHelper.toPdf(Paths.get("src/test/resources/n.ofd"), Paths.get("target/n.pdf"));
+            ConvertHelper.toPdf(Paths.get("C:\\Users\\pc\\Desktop\\error1.ofd"), Paths.get("target/n.pdf"));
 
         } catch (GeneralConvertException e) {
             e.printStackTrace();
