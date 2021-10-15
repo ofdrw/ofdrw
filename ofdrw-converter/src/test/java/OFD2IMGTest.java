c8b78c2075da61cd192c8725324590ad9bf4ccaf
cliven <quanguanyu@qq.com>
2021-10-15 20:36:13 +0800

    移除本地路径测试用例

--- ofdrw-converter/src/test/java/OFD2IMGTest.java
+++ ofdrw-converter/src/test/java/OFD2IMGTest.java
@@ -35,8 +35,6 @@ public class OFD2IMGTest {
 //        toPng("src/test/resources/不规范资源路径.ofd", "target/不规范资源路径.ofd");
 //        toPng("src/test/resources/V4RideRight.ofd", "target/V4RideRight.ofd");
 //        toPng("src/test/resources/发票示例.ofd", "target/发票示例.ofd");
-        toPng("C:\\Users\\pc\\Desktop\\ks\\ks.ofd", "target/ks.ofd");
-
         System.out.printf(">> 总计花费: %dms\n", System.currentTimeMillis() - start);
     }
 
