ca1d56d9fb89687ab365ee961f6f634df415b1cb
cliven <quanguanyu@qq.com>
2022-03-10 21:49:03 +0800

    修复了parent路径为null造成NPE

--- ofdrw-layout/src/main/java/org/ofdrw/layout/OFDDoc.java
+++ ofdrw-layout/src/main/java/org/ofdrw/layout/OFDDoc.java
@@ -158,8 +158,9 @@ public class OFDDoc implements Closeable {
         if (Files.isDirectory(outPath)) {
             throw new IllegalArgumentException("OFD文件存储路径(outPath)不能是目录");
         }
-        if (!Files.exists(outPath.getParent())) {
-            throw new IllegalArgumentException("OFD文件存储路径(outPath)上级目录 [" + outPath.getParent().toAbsolutePath() + "] 不存在");
+        final Path parent = outPath.getParent();
+        if (parent == null || !Files.exists(parent)) {
+            throw new IllegalArgumentException("OFD文件存储路径(outPath)上级目录 [" + parent + "] 不存在");
         }
         this.outPath = outPath;
     }
