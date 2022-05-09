6679384f9f0308d3a3561f9a64c1fb55a7065b3e
xiangyanggong <2418139356@qq.com>
2022-05-09 10:46:32 +0800

    修复XXE & zip slip 漏洞

--- ofdrw-reader/src/main/java/org/ofdrw/reader/ZipUtil.java
+++ ofdrw-reader/src/main/java/org/ofdrw/reader/ZipUtil.java
@@ -138,7 +138,12 @@ public class ZipUtil {
             Enumeration<? extends ZipEntry> entries = zipFile.entries();
             while (entries.hasMoreElements()) {
                 entry = entries.nextElement();
-                File f = new File(pathFile, entry.getName());
+
+                File f = new File(pathFile, entry.getName()).getCanonicalFile();
+
+                //校验路径合法性
+                pathValid(pathFile.getAbsolutePath(), f.getAbsolutePath());
+
                 if (entry.isDirectory()) {
                     if (!f.isDirectory() && !f.mkdirs()) {
                         throw new IOException("failed to create directory " + f);
