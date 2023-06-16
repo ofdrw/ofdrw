9b3d26398af4557067c4db3fb38600efef2f8184
王秋生 <qsh.wang@pcitc.com>
2023-06-16 09:29:22 +0800

    如果模板ID一样，但是模板内容不一样

--- ofdrw-tool/src/main/java/org/ofdrw/tool/merge/OFDMerger.java
+++ ofdrw-tool/src/main/java/org/ofdrw/tool/merge/OFDMerger.java
@@ -250,7 +250,10 @@ public class OFDMerger implements Closeable {
         CT_TemplatePage templatePage = tplPageMap.get(oldId);
         if (templatePage != null) {
             // 页面已经复制过
-            return templatePage.getID().ref();
+        	//如果模板ID一样，但是模板内容不一样
+        	if(templatePage.asXML().equals(tplObj.asXML())) {
+        		return templatePage.getID().ref();
+        	}
         }
 
         // 从文档中加载模板页面实体
