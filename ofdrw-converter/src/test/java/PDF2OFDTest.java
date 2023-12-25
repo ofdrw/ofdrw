8aa4a2eeb1d65558eb8de7bf7c390c1ecec628ef
lqc520 <lqc7478@gmail.com>
2023-12-25 16:34:30 +0800

    添加pdf转ofd流输出

--- ofdrw-converter/src/test/java/PDF2OFDTest.java
+++ ofdrw-converter/src/test/java/PDF2OFDTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.ofdrw.graphics2d.OFDGraphicsDocument;
 import org.ofdrw.graphics2d.OFDPageGraphics2D;
 
+import java.io.ByteArrayOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,10 +25,33 @@ public class PDF2OFDTest {
             PDFRenderer pdfRender = new PDFRenderer(pdfDoc);
             for (int pageIndex = 0; pageIndex < pdfDoc.getNumberOfPages(); pageIndex++) {
                 PDRectangle pdfPageSize = pdfDoc.getPage(pageIndex).getBBox();
-                OFDPageGraphics2D ofdPageG2d = ofdDoc.newPage(pdfPageSize.getWidth(), pdfPageSize.getHeight() );
+                OFDPageGraphics2D ofdPageG2d = ofdDoc.newPage(pdfPageSize.getWidth(), pdfPageSize.getHeight());
                 pdfRender.renderPageToGraphics(pageIndex, ofdPageG2d);
             }
         }
+
+    }
+
+
+    @Test
+    void pdfbox2ofdrwStream() throws Exception {
+
+        Path path = Paths.get("src/test/resources/Test.pdf");
+        Path dir = Paths.get("target", path.getFileName().toString());
+
+        Files.createDirectories(dir);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (OFDGraphicsDocument ofdDoc = new OFDGraphicsDocument(outputStream);
+             PDDocument pdfDoc = PDDocument.load(path.toFile())) {
+            PDFRenderer pdfRender = new PDFRenderer(pdfDoc);
+            for (int pageIndex = 0; pageIndex < pdfDoc.getNumberOfPages(); pageIndex++) {
+                PDRectangle pdfPageSize = pdfDoc.getPage(pageIndex).getBBox();
+                OFDPageGraphics2D ofdPageG2d = ofdDoc.newPage(pdfPageSize.getWidth(), pdfPageSize.getHeight());
+                pdfRender.renderPageToGraphics(pageIndex, ofdPageG2d);
+            }
+        }
+        Path out = Paths.get("target", "Test.ofd");
+        Files.write(out, outputStream.toByteArray());
 
     }
 }
