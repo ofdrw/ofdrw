04d20d5f416bbeeb3537c7d55f51e7331a5cfc3e
spenggch <spenggch@126.com>
2024-03-20 14:06:23 +0800

    增加ItextTrueTypeFont类，不再通过反射对裁剪字体进行处理

--- ofdrw-converter/src/main/java/com/itextpdf/io/font/ItextFontUtil.java
+++ ofdrw-converter/src/main/java/com/itextpdf/io/font/ItextFontUtil.java
@@ -1,7 +1,6 @@
 package com.itextpdf.io.font;
 
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -12,11 +11,6 @@ import java.nio.file.Paths;
  *
  */
 public class ItextFontUtil {
-
-    private static Constructor<TrueTypeFont> TrueTypeFontConstructor;
-    static {
-        TrueTypeFontConstructor = getConstructor();
-    }
 
     private ItextFontUtil() {}
 
@@ -53,75 +47,22 @@ public class ItextFontUtil {
     /**
      * 加载字体
      * 
-     * @param fontName 字体文件名称或全路径
      * @param fontRaw 字体文件内容
      * @return
      * @throws IOException
      */
-    public static FontProgram loadFontProgram(String fontName, byte[] fontRaw) throws IOException {
-        if (fontRaw != null && fontRaw.length > 0) {
-            FontProgram fontProgram = null;
-            final String fileName = fontName.toLowerCase();
-            try {
-                if (fileName.endsWith(".ttc")) {
-                    fontProgram = FontProgramFactory.createFont(fontRaw, 0, false);
-                } else if (fileName.endsWith(".ttf") || fileName.endsWith(".otf")) {
-                    fontProgram = loadFont(fontRaw);
-                } else {
-                    fontProgram = FontProgramFactory.createFont(fontRaw);
-                }
-                return fontProgram;
-            } catch (IOException e) {
-                throw e;
-            } catch (Exception e) {
-                throw new IOException(e);
-            }
-        } else {
-            return loadFontProgram(fontName);
-        }
-    }
-
-    /**
-     * 加载字体
-     * 
-     * @param fontRaw 字体文件内容
-     * @return
-     * @throws IOException
-     */
-    public static TrueTypeFont loadFont(byte[] fontRaw) throws Exception {
+    public static TrueTypeFont loadFont(byte[] fontRaw) throws IOException {
         try {
             return new TrueTypeFont(fontRaw);
         } catch (Exception e1) {
             try {
-                return loadFont(new ItextOpenTypeParser(fontRaw));
-            } catch (Exception e2) {
+                return new ItextTrueTypeFont(fontRaw);
+            } catch (IOException e2) {
                 throw e2;
+            } catch (Exception e2) {
+                throw new IOException(e2);
             }
         }
-    }
-
-    private static TrueTypeFont loadFont(OpenTypeParser fontParser) throws Exception {
-        return TrueTypeFontConstructor.newInstance(fontParser);
-    }
-
-    @SuppressWarnings("unchecked")
-    private static Constructor<TrueTypeFont> getConstructor() {
-        Constructor<?>[] constructors = TrueTypeFont.class.getDeclaredConstructors();
-        for (Constructor<?> constructor : constructors) {
-            if (constructor.getParameterCount() != 1) {
-                continue;
-            }
-            if (constructor.getParameterTypes()[0].isAssignableFrom(OpenTypeParser.class)) {
-                try {
-                    if (!constructor.isAccessible()) {
-                        constructor.setAccessible(true);
-                    }
-                    return (Constructor<TrueTypeFont>)constructor;
-                } catch (Exception e) {
-                }
-            }
-        }
-        return null;
     }
 
 }
