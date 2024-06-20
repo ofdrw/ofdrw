33cce1014c9263176d1320e3aff1d9c0af35388c
spenggch <spenggch@126.com>
2024-06-20 17:31:17 +0800

    修复 scaling 方法的判断逻辑，解决印章的比例尺问题，使用测试用例 PDFExporterITextTest->export 可以进行相关验证

--- ofdrw-converter/src/main/java/org/ofdrw/converter/ItextMaker.java
+++ ofdrw-converter/src/main/java/org/ofdrw/converter/ItextMaker.java
@@ -642,8 +642,8 @@ public class ItextMaker {
      */
     private double scaling(ST_Box targetBox, @SuppressWarnings("rawtypes") CT_GraphicUnit graphicUnit) {
         double scale = 1D;
-        PageBlockType instance = PageBlockType.getInstance(graphicUnit);
-        if (instance != null) {
+        PageBlockType instance = PageBlockType.getInstance(graphicUnit.getParent());
+        if (instance != null && instance instanceof CT_PageBlock) {
             scale = scaling(targetBox, graphicUnit.getBoundary());
         }
         return scale;
